d2c11e315f9875b2758096e5acd75d0987f4e439
fraz0815 <l.schimmel83@gmail.com>
2024-09-28 21:27:20 +0200

    remove x11, add check for user in input group

--- gptouch.py
+++ gptouch.py
@@ -14,25 +14,11 @@ def check_command(command, name):
 
 def check_dependencies():
     if os.environ.get("XDG_SESSION_TYPE") == "x11":
-        check_command("xrandr", "xrandr")
-        check_command("xinput", "xinput")
+         print("x11 not supported, Wayland only")
+         exit(1)
     else:
         check_command("gnome-randr", "gnome-randr")
         check_command("libinput", "libinput")
-
-def get_active_output_x11():
-    try:
-        result = subprocess.run(["xrandr", "--listactivemonitors"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = result.stdout.decode()
-        matches = re.findall(r'\d+: \+\*?(\S+)', output)
-        if matches:
-            return matches[0]
-        else:
-            print("No active output found. Please check your display connections.")
-            exit(1)
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to get active output: {str(e)}")
-        exit(1)
 
 def get_active_output_wayland():
     try:
@@ -48,26 +34,6 @@ def get_active_output_wayland():
         print(f"Failed to get active output: {str(e)}")
         exit(1)
 
-def get_active_output():
-    if os.environ.get("XDG_SESSION_TYPE") == "x11":
-        return get_active_output_x11()
-    else:
-        return get_active_output_wayland()
-
-def get_touchscreen_device_x11():
-    try:
-        result = subprocess.run(["xinput", "list", "--name-only"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output = result.stdout.decode()
-        matches = re.findall(r'(.*TouchScreen.*)', output, re.IGNORECASE)
-        if matches:
-            return matches[0]
-        else:
-            print("No touchscreen device found. Please check your connections.")
-            exit(1)
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to get touchscreen device: {str(e)}")
-        exit(1)
-
 def get_touchscreen_device_wayland():
     try:
         result = subprocess.run(["libinput", "list-devices"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -76,17 +42,12 @@ def get_touchscreen_device_wayland():
         if matches:
             return matches[0].split(":")[1].strip()
         else:
-            print("No touchscreen device found. Please check your connections.")
+            print("No touchscreen device found. Please check your connections .")
+            print("Also add/check if user in the 'input' group, see Readme for details")
             exit(1)
     except subprocess.CalledProcessError as e:
         print(f"Failed to get touchscreen device: {str(e)}")
         exit(1)
-
-def get_touchscreen_device():
-    if os.environ.get("XDG_SESSION_TYPE") == "x11":
-        return get_touchscreen_device_x11()
-    else:
-        return get_touchscreen_device_wayland()
 
 def select_orientation():
     print("Select screen orientation:")
@@ -117,31 +78,25 @@ def update_calibration_matrix(choice):
 
 def main():
     check_dependencies()
-    OUTPUT_DISPLAY_DEFAULT = get_active_output()
-    TOUCHSCREEN_DEVICE_DEFAULT = get_touchscreen_device()
+    OUTPUT_DISPLAY_DEFAULT = get_active_output_wayland()
+    TOUCHSCREEN_DEVICE_DEFAULT = get_touchscreen_device_wayland()
     
     choice = select_orientation()
     if choice not in [1, 2, 3, 4]:
         print("Invalid choice. Exiting...")
         return
-
+  
     rotation, calibration_matrix = update_calibration_matrix(choice)
     if not rotation:
         print("Invalid choice. Exiting...")
         return
 
-    if os.environ.get("XDG_SESSION_TYPE") == "x11":
-        # Rotate the display using xrandr
-        subprocess.run(["xrandr", "--output", OUTPUT_DISPLAY_DEFAULT, "--rotate", rotation])
-    else:
-        # Rotate the display using gnome-randr
-        subprocess.run(["gnome-randr", "modify", OUTPUT_DISPLAY_DEFAULT, "--rotate", rotation, "--persistent"])
-    
+    subprocess.run(["gnome-randr", "modify", OUTPUT_DISPLAY_DEFAULT, "--rotate", rotation, "--persistent"])   
     print()
     subprocess.run(["sudo", "tee", "/etc/udev/rules.d/99-touchscreen-orientation.rules"], input=f'ATTRS{{name}}=="{TOUCHSCREEN_DEVICE_DEFAULT}", ENV{{LIBINPUT_CALIBRATION_MATRIX}}="{calibration_matrix}"'.encode())
-
     print()
-    print("On X11 switch touchscreen off/on instead of reboot")
+    print('optional use "gdm-settings" for applying rotation to GDM/login screen')
+    print()
     reboot = input("Reboot now? (y/n): ").strip().lower()
     if reboot == 'y':
         subprocess.run(["sudo", "reboot"])
